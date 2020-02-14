3c6b8c014fe2572b9c798026399c3a1193ce4689
spyentist <spyentist@yahoo.com>
2020-02-13 22:36:23 -0700

    Edits for Week 6

--- week_6/scripts/script.js
+++ week_6/scripts/script.js
@@ -46,25 +46,46 @@ function doInputOutput() {
     document.getElementById("tempL5").innerHTML = parseInt(Math.random() * 10);
 }
 
+
+function weatherConditions() {
+    var temp, wind, humid, windchill;
+
+    temp =parseInt(Math.random()*100);
+    wind = parseInt(Math.random()*10);
+    tempdeg = temp + "&deg;";
+    windspeed = wind + "mph";
+    humid = "15" + "&percnt;";
+    
+    
+    if(temp <= 50 && wind >= 3) {
+        windchill = 35.74 +  0.6215 * temp - 35.75 * Math.pow(wind, 0.16) + 0.4275 * temp * Math.pow(wind, 0.16);
+        windchilldisplay = windchill.toFixed(0) + '&deg;'
+    } else {
+        document.querySelector("#winddiv").style.display="none";
+    }
+    
+    document.getElementById("curTemp").innerHTML = tempdeg;
+    document.getElementById("windSpeed").innerHTML = windspeed;
+    document.getElementById("humidity").innerHTML = humid;
+    document.getElementById("windchill").innerHTML = windchilldisplay;
+}
+
 function adaptiveMenu() {
     var links = document.getElementById("navigation");
-    if (links.className === "topnav") {
+    if (links.className === "navigation") {
         links.className += ".responsive";
     } else {
-        links.className = "topnav";
+        links.className = "navigation";
     }
     
 }
 
-function weatherConditions() {
-    var temp, wind, humid;
-
-    temp = "50" + "&deg;";
-    wind = "10" + "mph";
-    humid = "15" + "&percnt;";
-    
-    
-    document.getElementById("curTemp").innerHTML = temp;
-    document.getElementById("windSpeed").innerHTML = wind;
-    document.getElementById("humidity").innerHTML = humid;
-}
+WebFont.load({
+    google: {
+      families: [
+         'Bebas Neue',
+         'Lato',
+         'Yanone Kaffeesatz'
+      ]
+    }
+  });
