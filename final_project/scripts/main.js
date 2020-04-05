33dfb6ee0cd101bc463e63feb8ccd624b7667bd9
spyentist <spyentist@yahoo.com>
2020-04-04 21:11:55 -0600

    editing js

--- final_project/scripts/main.js
+++ final_project/scripts/main.js
@@ -91,33 +91,62 @@ function getTempleFull() {
 function buildFull(temple) {
 
     let card = document.createElement('article');
-    let subcard = document.createElement('section');
+    let info = document.createElement('section');
+    let address = document.createElement('section');
+    let weather = document.createElement('section');
     let h1 = document.createElement('h1');
+    let infoh3 = document.createElement("h3");
     let announce = document.createElement('p');
+    let groundbreaking = document.createElement('p');
     let dedicated = document.createElement('p');
+    let reded1 = document.createElement('p');
+    let reded2 = document.createElement('p');
+    let addressh3 = document.createElement('h3');
     let stradd1 = document.createElement('p');
     let stradd2 = document.createElement('p');
+    let addr3 = document.createElement('p');
     let country = document.createElement('p');
     let image = document.createElement('img');
-    let br = document.createElement('br');
+    // let br = document.createElement('br');
 
     h1.textContent = temple.Name + " Temple";
-    announce.textContent =  temple.History[0].Milestone + ": " + temple.History[0].Date;
-    dedicated.textContent = temple.History[2].Milestone + ": " + temple.History[2].Date;
+    announce.textContent =  temple.History[0].Milestone + temple.History[0].Date;
+    groundbreaking.textContent = temple.History[1].Milestone + temple.History[1].Date;
+    dedicated.textContent = temple.History[2].Milestone + temple.History[2].Date;
+    reded1.textContent = temple.History[3].Milestone + temple.History[3].Date;
+    reded2.textContent = temple.History[4].Milestone + temple.History[4].Date;
+    stradd1.textContent = temple.StreetLine1;
+    stradd2.textContent = temple.StreetLine2;
+    addr3.textContent = temple.City + ", " + temple.State + " " +  temple.Zip;
     country.textContent =  temple.Country;
     image.setAttribute('src', "images/" + temple.Photo);
     image.setAttribute('alt', temple.Name + " Photo");
+    infoh3.textContent = 'Fun Facts';
+    addressh3.textContent = 'Address';
+
 
 
     card.appendChild(h1);
     card.appendChild(image);
-    card.appendChild(announce);
-    card.appendChild(dedicated);
-    // card.appendChild(br);
-    card.appendChild(country);
+    card.appendChild(info);
+        info.appendChild(infoh3);
+        info.appendChild(announce);
+        info.appendChild(groundbreaking);
+        info.appendChild(dedicated);
+        info.appendChild(reded1);
+        info.appendChild(reded2);
+    card.appendChild(address);
+        address.appendChild(addressh3);
+        address.appendChild(stradd1);
+        address.appendChild(stradd2);
+        address.appendChild(addr3);
+        address.appendChild(country);
 
     card.setAttribute('class', 'temple_card');
     card.setAttribute('id', temple.Photo + "_card");
+        info.setAttribute("class", 'information');
+        address.setAttribute('class', 'address');
+        weather.setAttribute('class', 'weather');
 
     document.querySelector('#templefull').appendChild(card);
 }
